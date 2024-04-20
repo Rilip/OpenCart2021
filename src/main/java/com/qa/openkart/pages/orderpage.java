ad6473ef1c35cc13bdd3d81730f20189ccace337
Rilip <rdilipkumar124@gmail.com>
2024-04-20 16:45:50 +0530

    updated  order page as per given review comments by dilip

--- src/main/java/com/qa/openkart/pages/orderpage.java
+++ src/main/java/com/qa/openkart/pages/orderpage.java
@@ -1,10 +1,13 @@
 package com.qa.openkart.pages;
 
+import org.openqa.selenium.By;
+
 public class orderpage {
 
 	String order;
+	private By check =By.id("check");
 	 
-	public void order() {
+	public void orderproduct() {
 		System.out.println("Order id:-12345");
 	}
 }
